f21e6325b75437dcfe9835b2adb7d2108020f2b8
Luciano Freire <lucianofreiredigital29@gmail.com>
2026-09-12 10:53:26 -0300

    feat: adiciona tipos da coleção

---
+++ src/types/collection.ts
@@ -0,0 +1,12 @@
+export type CollectionCatalogItem = {
+  aniimoNumero: number;
+  aniimoNome: string;
+  aniimoSlug: string;
+  funcao: string;
+  estagio: string | null;
+  formaId: number;
+  formaNome: string;
+  formaSlug: string;
+  imagemUrl: string | null;
+  elementos: string[];
+};
